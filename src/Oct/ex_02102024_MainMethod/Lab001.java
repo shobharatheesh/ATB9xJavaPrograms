aa89469565879bbe2be6256410e37d684a4b2ba0
Shobha Rao <sraork@gmail.com>
2024-10-06 13:21:20 +0530

    updated  f

--- src/Oct/ex_02102024_MainMethod/Lab001.java
+++ src/Oct/ex_02102024_MainMethod/Lab001.java
@@ -2,7 +2,7 @@ package Oct.ex_02102024_MainMethod;
 
 public class Lab001 {
     public static void main(String[] args) {
-       System.out.println("Hello in welcome Java");
+       System.out.println("Hello indi welcome Java");
 
     }
 }
