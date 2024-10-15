6ee8ba72ec11886c7853c60dd860febf4677ceab
Shobha Rao <sraork@gmail.com>
2024-10-15 22:25:53 +0530

    Changes 15/10/2024

---
+++ src/Oct_JavaCourse_ATB9X/ex_15102024/Operators.java
@@ -0,0 +1,12 @@
+package Oct_JavaCourse_ATB9X.ex_15102024;
+
+public class Operators {
+
+    public static void main(String[] args) {
+
+        char a1 = 'F';
+        char b1 = 'G';
+
+        System.out.println(a1+b1); //Concatenation of char results in int values = 70 + 71
+    }
+}
---
+++ src/Oct_JavaCourse_ATB9X/ex_15102024/Operators2.java
@@ -0,0 +1,12 @@
+package Oct_JavaCourse_ATB9X.ex_15102024;
+
+public class Operators2 {
+
+    public static void main(String[] args) {
+
+        short s = 10;
+        char c = 'A';
+
+        System.out.println(s+c);
+    }
+}
