ac3efb06e405b148fcd778f5b755dab2a4e08797
Shobha Rao <sraork@gmail.com>
2024-10-15 15:38:37 +0530

    Changes 15/10/2024

---
+++ src/Oct_JavaCourse_ATB9X/Task15102024/Modulo.java
@@ -0,0 +1,13 @@
+package Oct_JavaCourse_ATB9X.Task15102024;
+
+public class Modulo {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 12;
+
+        System.out.println(a % b);
+
+    }
+}
--- src/Oct_JavaCourse_ATB9X/Task15102024/OperandIncreDecre.java
+++ src/Oct_JavaCourse_ATB9X/Task15102024/OperandIncreDecre.java
@@ -7,5 +7,8 @@ public class OperandIncreDecre {
         int a= 20;
         System.out.println( --a + a++ + a--);
         System.out.println(a);
+
     }
 }
+
+
